4104abe49da86e64d6100815b6ae350f6a274801
ljh <huningyo@naver.com>
2021-04-30 10:33:02 +0900

    test

---
+++ application/controllers/sa/Login.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once(APPPATH ."controllers/base/Sa_base.php");
+
+class Login extends Sa_base {
+
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function index(){
+
+        echo 1;
+        exit;
+
+    }
+
+}
