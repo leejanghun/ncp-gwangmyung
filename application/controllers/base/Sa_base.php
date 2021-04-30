44aecca2e2cec5a76c3d322598a1692a51ac54d5
ljh <huningyo@naver.com>
2021-04-30 10:33:19 +0900

    test

---
+++ application/controllers/base/Sa_base.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once(APPPATH ."controllers/base/Common_base.php");
+
+class Sa_base extends Common_base{
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+
+}
